462239237db4ca5402c8b8618fd9ad6827f5bed2
ingrid <ingridledesma22@gmail.com>
2025-08-12 11:38:38 -0300

    seccion programas se agrega fonfo para q queden igual todas las secciones

--- resources/views/layouts/programas.blade.php
+++ resources/views/layouts/programas.blade.php
@@ -133,6 +133,22 @@
         </header>
     </div>
 
+    <header class="masthead autorizacion">
+        <div class="container px-4 px-lg-5 h-100">
+            <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                <div class="col-lg-8 align-self-end">
+                    <h1 class="text-white font-weight-bold">Programas Vigentes</h1>
+
+                </div>
+                <div class="col-lg-8 align-self-baseline">
+                    <p class="text-white-75 mb-5">
+                        Enterate de los programas vigentes a los cuales podes acceder
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
+
 
 
     <!-- End Header -->
